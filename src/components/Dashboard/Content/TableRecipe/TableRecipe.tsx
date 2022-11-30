50b73fbe10d212f6c501a27378c48af9a6ad6986
pollylnd <pollylnd@outlook.com>
2022-11-30 21:47:18 +0100

    fix

--- src/components/Dashboard/Content/TableRecipe/TableRecipe.tsx
+++ src/components/Dashboard/Content/TableRecipe/TableRecipe.tsx
@@ -1,270 +1,249 @@
-import React, { useState } from 'react';
-import {styled} from "@mui/system";
-import ratingImg from './rating.svg'
+import React, { useState } from "react";
+import { styled } from "@mui/system";
+import ratingImg from "src/images/svg/rating.svg";
 
-const BlockTableRecipe = styled('div')({
-    width: "19.2%",
-    borderRadius: "6px",
-    border: "0.5px solid #153453",
-    background: "#081A2C",
-    margin: "17px 0px 0px 18px",
-    padding: "29px 28px 31px 28px",
-    "@media (max-width:1900px)": {
-        width: "19.5%",
-        margin: "15px 0px 0px 14px",
-        padding: "19px 19px",
-    }
+const BlockTableRecipe = styled("div")({
+  width: "19.2%",
+  borderRadius: "6px",
+  border: "0.5px solid #153453",
+  background: "#081A2C",
+  margin: "17px 0px 0px 18px",
+  padding: "29px 28px 31px 28px",
+  "@media (max-width:1900px)": {
+    width: "19.5%",
+    margin: "15px 0px 0px 14px",
+    padding: "19px 19px",
+  },
 });
 
-const BlockName = styled('span')({
-    width: "147px",
-    fontWeight: "500",
-    fontSize: "18px",
-    lineHeight: "21px",
-    color: "#FFFFFF"
+const BlockName = styled("span")({
+  width: "147px",
+  fontWeight: "500",
+  fontSize: "18px",
+  lineHeight: "21px",
+  color: "#FFFFFF",
 });
 
-const TableRecipeBlocks = styled('div')({
-    display: "flex",
-    justifyContent: "space-between",
-    marginTop: "28px",
-    marginBottom: "21px"
+const TableRecipeBlocks = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
+  marginTop: "28px",
+  marginBottom: "21px",
 });
 
-const BlockDishes = styled('div')({
-    textAlign: "center",
-    width: "37%",
-    height: "136px",
-    background: "#061321",
-    borderRadius: "10px",
-    "@media (max-width:1900px)": {
-        width: "44%",
-        height: "100px",
-    }
+const BlockDishes = styled("div")({
+  textAlign: "center",
+  width: "37%",
+  height: "136px",
+  background: "#061321",
+  borderRadius: "10px",
+  "@media (max-width:1900px)": {
+    width: "44%",
+    height: "100px",
+  },
 });
 
-const BlockNumber = styled('div')({
-    marginTop: "24px",
-    marginBottom: "21px",
-    "@media (max-width:1900px)": {
-        marginTop: "10px",
-        marginBottom: "11px",
-    }
+const BlockNumber = styled("div")({
+  marginTop: "24px",
+  marginBottom: "21px",
+  "@media (max-width:1900px)": {
+    marginTop: "10px",
+    marginBottom: "11px",
+  },
 });
 
-const BlockNumberSpan = styled('span')({
-    fontStyle: "normal",
-    fontWeight: "500",
-    fontSize: "30px",
-    lineHeight: "35px",
-    color: "#FC224F"
+const BlockNumberSpan = styled("span")({
+  fontStyle: "normal",
+  fontWeight: "500",
+  fontSize: "30px",
+  lineHeight: "35px",
+  color: "#FC224F",
 });
 
-const TableRecipeBlockVeg = styled('div')({
-    marginTop: "24px",
-    marginBottom: "21px",
+const TableRecipeBlockVeg = styled("div")({
+  marginTop: "24px",
+  marginBottom: "21px",
 });
 
-const BlockVegSpan = styled('span')({
-    textAlign: "center",
-    fontStyle: "normal",
-    fontWeight: "400",
-    fontSize: "14px",
-    lineHeight: "16px",
-    color: "#7E89A8"
+const BlockVegSpan = styled("span")({
+  textAlign: "center",
+  fontStyle: "normal",
+  fontWeight: "400",
+  fontSize: "14px",
+  lineHeight: "16px",
+  color: "#7E89A8",
 });
 
-const BlockNumberSpanNonVeg = styled('span')({
-    fontStyle: "normal",
-    fontWeight: "500",
-    fontSize: "30px",
-    lineHeight: "35px",
-    color: "#14CC74"
+const BlockNumberSpanNonVeg = styled("span")({
+  fontStyle: "normal",
+  fontWeight: "500",
+  fontSize: "30px",
+  lineHeight: "35px",
+  color: "#14CC74",
 });
 
-const BlockPopulars = styled('span')({
-    fontStyle: "normal",
-    fontWeight: "500",
-    fontSize: "14px",
-    lineHeight: "16px",
-    color: "#7E89A8"
+const BlockPopulars = styled("span")({
+  fontStyle: "normal",
+  fontWeight: "500",
+  fontSize: "14px",
+  lineHeight: "16px",
+  color: "#7E89A8",
 });
 
-const PopularsBody = styled('div')({
-    marginTop: "28px",
-    "@media (max-width:1900px)": {
-        marginTop: "10px"
-    }
+const PopularsBody = styled("div")({
+  marginTop: "28px",
+  "@media (max-width:1900px)": {
+    marginTop: "10px",
+  },
 });
 
-const PopularsItems = styled('div')({
-    display: "flex",
-    justifyContent: "space-between",
+const PopularsItems = styled("div")({
+  display: "flex",
+  justifyContent: "space-between",
 });
 
-const PopularsItemLeft = styled('div')({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
+const PopularsItemLeft = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 });
 
-const PopularsItem = styled('div')({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "28px",
-    height: "28px",
-    border: "1px solid #4B2C57",
-    borderRadius: "6px"
+const PopularsItem = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "28px",
+  height: "28px",
+  border: "1px solid #4B2C57",
+  borderRadius: "6px",
 });
 
-const PopularsItemSpan = styled('span')({
-    fontSize: "12px",
-    lineHeight: "14px",
-    color: "#FA59C7"
+const PopularsItemSpan = styled("span")({
+  fontSize: "12px",
+  lineHeight: "14px",
+  color: "#FA59C7",
 });
 
-const PopularsItemName = styled('div')({
-    marginLeft: "12px",
-    width: "88px",
-    fontSize: "12px",
-    lineHeight: "14px",
-    color: "#7E89A8"
+const PopularsItemName = styled("div")({
+  marginLeft: "12px",
+  width: "88px",
+  fontSize: "12px",
+  lineHeight: "14px",
+  color: "#7E89A8",
 });
 
-const PopularsItemRight = styled('div')({
-    display: "flex",
-    alignItems: "center"
+const PopularsItemRight = styled("div")({
+  display: "flex",
+  alignItems: "center",
 });
 
-const PopularsItemRightImg = styled('div')({
-    display: "flex",
-    alignItems: "center"
+const PopularsItemRightImg = styled("div")({
+  display: "flex",
+  alignItems: "center",
 });
 
-const PopularsItemRightRatingImg = styled('img')({});
+const PopularsItemRightRatingImg = styled("img")({});
 
-const PopularsItemRightRating = styled('div')({
-    marginLeft: "7px",
-    fontSize: "12px",
-    lineHeight: "14px",
-    color: "#7E89A8"
+const PopularsItemRightRating = styled("div")({
+  marginLeft: "7px",
+  fontSize: "12px",
+  lineHeight: "14px",
+  color: "#7E89A8",
 });
 
-const TableRecipeLine = styled('div')({
-    marginTop: "19px",
-    marginBottom: "37px",
-    width: "100%",
-    border: "0.5px dashed #2A527A",
-    "@media (max-width:1900px)": {
-        marginTop: "14px",
-        marginBottom: "14px",
-    }
+const TableRecipeLine = styled("div")({
+  marginTop: "19px",
+  marginBottom: "37px",
+  width: "100%",
+  border: "0.5px dashed #2A527A",
+  "@media (max-width:1900px)": {
+    marginTop: "14px",
+    marginBottom: "14px",
+  },
 });
 
-const TableRecipeLineView = styled('div')({
-    display: "flex",
-    justifyContent: "end"
+const TableRecipeLineView = styled("div")({
+  display: "flex",
+  justifyContent: "end",
 });
 
-const TableRecipeLineViewButton = styled('button')({
-    width: "36.5%",
-    padding: "7px",
-    borderRadius: "5px",
-    background: "#073374",
-    color: "#FFFFFF",
-    cursor: "pointer",
-    border: "none"
+const TableRecipeLineViewButton = styled("button")({
+  width: "36.5%",
+  padding: "7px",
+  borderRadius: "5px",
+  background: "#073374",
+  color: "#FFFFFF",
+  cursor: "pointer",
+  border: "none",
 });
 
 const TableRecipe = () => {
-    const dishes = [
-        {number: "01", name: "Caper and chickpea pasta", rating: "4.5"},
-        {number: "02", name: "Fish and pasta casserole", rating: "4.5"},
-        {number: "03", name: "Squash and coconut bread", rating: "4.5"},
-        {number: "04", name: "Squash and coconut bread", rating: "4.5"}
-    ]
+  const dishes = [
+    { number: "01", name: "Caper and chickpea pasta", rating: "4.5" },
+    { number: "02", name: "Fish and pasta casserole", rating: "4.5" },
+    { number: "03", name: "Squash and coconut bread", rating: "4.5" },
+    { number: "04", name: "Squash and coconut bread", rating: "4.5" },
+  ];
 
-    const [veg, setVeg] = useState<string>("35")
-    const [nonVeg, setNonVeg] = useState<string>("29")
+  const [veg, setVeg] = useState<string>("35");
+  const [nonVeg, setNonVeg] = useState<string>("29");
 
-    return (
-        <BlockTableRecipe>
-            <BlockName>
-                Todays Recipe/Dish
-            </BlockName>
+  return (
+    <BlockTableRecipe>
+      <BlockName>Todays Recipe/Dish</BlockName>
 
-            <TableRecipeBlocks>
-                <BlockDishes>
-                    <BlockNumber>
-                        <BlockNumberSpan>
-                            {veg}
-                        </BlockNumberSpan>
-                    </BlockNumber>
+      <TableRecipeBlocks>
+        <BlockDishes>
+          <BlockNumber>
+            <BlockNumberSpan>{veg}</BlockNumberSpan>
+          </BlockNumber>
 
-                    <TableRecipeBlockVeg>
-                        <BlockVegSpan>
-                            Non-Veg Dishes
-                        </BlockVegSpan>
-                    </TableRecipeBlockVeg>
-                </BlockDishes>
+          <TableRecipeBlockVeg>
+            <BlockVegSpan>Non-Veg Dishes</BlockVegSpan>
+          </TableRecipeBlockVeg>
+        </BlockDishes>
 
-                <BlockDishes>
-                    <BlockNumber>
-                        <BlockNumberSpanNonVeg>
-                            {nonVeg}
-                        </BlockNumberSpanNonVeg>
-                    </BlockNumber>
+        <BlockDishes>
+          <BlockNumber>
+            <BlockNumberSpanNonVeg>{nonVeg}</BlockNumberSpanNonVeg>
+          </BlockNumber>
 
-                    <TableRecipeBlockVeg>
-                        <BlockVegSpan>
-                            Veg Dishes
-                        </BlockVegSpan>
-                    </TableRecipeBlockVeg>
-                </BlockDishes>
+          <TableRecipeBlockVeg>
+            <BlockVegSpan>Veg Dishes</BlockVegSpan>
+          </TableRecipeBlockVeg>
+        </BlockDishes>
+      </TableRecipeBlocks>
 
-            </TableRecipeBlocks>
+      <BlockPopulars>Popular Dishes</BlockPopulars>
 
-            <BlockPopulars>
-                Popular Dishes
-            </BlockPopulars>
+      {dishes.map((dish, index) => (
+        <PopularsBody key={index}>
+          <PopularsItems>
+            <PopularsItemLeft>
+              <PopularsItem>
+                <PopularsItemSpan>{dish.number}</PopularsItemSpan>
+              </PopularsItem>
+              <PopularsItemName>{dish.name}</PopularsItemName>
+            </PopularsItemLeft>
 
-            {dishes.map((dish, index) =>
-                <PopularsBody key={index}>
-                    <PopularsItems>
-                        <PopularsItemLeft>
-                            <PopularsItem>
-                                <PopularsItemSpan>
-                                    {dish.number}
-                                </PopularsItemSpan>
-                            </PopularsItem>
-                            <PopularsItemName>
-                                {dish.name}
-                            </PopularsItemName>
-                        </PopularsItemLeft>
+            <PopularsItemRight>
+              <PopularsItemRightImg>
+                <PopularsItemRightRatingImg src={ratingImg} alt="rating" />
+              </PopularsItemRightImg>
 
-                        <PopularsItemRight>
-                            <PopularsItemRightImg>
-                                <PopularsItemRightRatingImg src={ratingImg} alt="rating"/>
-                            </PopularsItemRightImg>
+              <PopularsItemRightRating>{dish.rating}</PopularsItemRightRating>
+            </PopularsItemRight>
+          </PopularsItems>
 
-                            <PopularsItemRightRating>
-                                {dish.rating}
-                            </PopularsItemRightRating>
-                        </PopularsItemRight>
-                    </PopularsItems>
+          <TableRecipeLine />
+        </PopularsBody>
+      ))}
 
-                    <TableRecipeLine/>
-                </PopularsBody>
-            )}
-
-            <TableRecipeLineView>
-                <TableRecipeLineViewButton>
-                    View All
-                </TableRecipeLineViewButton>
-            </TableRecipeLineView>
-        </BlockTableRecipe>
-    );
+      <TableRecipeLineView>
+        <TableRecipeLineViewButton>View All</TableRecipeLineViewButton>
+      </TableRecipeLineView>
+    </BlockTableRecipe>
+  );
 };
 
 export default TableRecipe;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled } from '@mui/system';
+import { styled } from "@mui/system";
 import NewOrders from "../../components/Dashboard/Content/NewOrders/NewOrders";
 import ProcessOrders from "../../components/Dashboard/Content/ProcessOrders/ProcessOrders";
 import DeliveredOrders from "../../components/Dashboard/Content/DeliveredOrders/DeliveredOrders";
@@ -10,51 +10,49 @@ import TableDetails from "../../components/Dashboard/Content/TableDetails/TableD
 import TableRecipe from "../../components/Dashboard/Content/TableRecipe/TableRecipe";
 import TableDishes from "../../components/Dashboard/Content/TableDishes/TableDishes";
 
-const ContainerDashboard = styled('div')({
-    minHeight: "calc(100vh - 64px)",
-    backgroundColor: '#030f1c',
+const ContainerDashboard = styled("div")({
+  minHeight: "calc(100vh - 64px)",
+  backgroundColor: "#030f1c",
 });
 
-const DashboardGrid = styled('div')({
-    marginLeft: "184px"
+const DashboardGrid = styled("div")({
+  marginLeft: "184px",
 });
 
-const DashboardUpBlock = styled('div')({
-    display: "flex",
-    height: "300px"
+const DashboardUpBlock = styled("div")({
+  display: "flex",
+  height: "300px",
 });
 
-const DashboardDownBlock = styled('div')({
-    display: "flex",
-    height: "600px",
-    "@media (max-width:1900px)":{
-        height: "525px",
-    }
+const DashboardDownBlock = styled("div")({
+  display: "flex",
+  height: "600px",
+  "@media (max-width:1900px)": {
+    height: "525px",
+  },
 });
 
 const Dashboard = () => {
-    return (
-        <ContainerDashboard>
+  return (
+    <ContainerDashboard>
+      <DashboardGrid>
+        <DashboardUpBlock>
+          <NewOrders />
+          <ProcessOrders />
+          <DeliveredOrders />
+          <TableBooking />
+          <LiveKitchen />
+        </DashboardUpBlock>
 
-            <DashboardGrid>
-                <DashboardUpBlock>
-                    <NewOrders/>
-                    <ProcessOrders/>
-                    <DeliveredOrders/>
-                    <TableBooking/>
-                    <LiveKitchen/>
-                </DashboardUpBlock>
-
-                <DashboardDownBlock>
-                    <SalesGraph/>
-                    <TableDetails/>
-                    <TableRecipe/>
-                    <TableDishes/>
-                </DashboardDownBlock>
-            </DashboardGrid>
-
-        </ContainerDashboard>
-    );
+        <DashboardDownBlock>
+          <SalesGraph />
+          <TableDetails />
+          <TableRecipe />
+          <TableDishes />
+        </DashboardDownBlock>
+      </DashboardGrid>
+    </ContainerDashboard>
+  );
 };
 
 export default Dashboard;
