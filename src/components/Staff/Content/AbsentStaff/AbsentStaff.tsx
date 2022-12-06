334afbf4800beac2beeca154ae5c5878f6afc8c5
evgeny3322 <retard_x_98@mail.ru>
2022-12-06 21:05:58 +0300

    add grid staff page

---
+++ src/components/Staff/Content/AbsentStaff/AbsentStaff.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { styled } from "@mui/system";
+
+const ContainerAbsentStaff = styled("div")({
+    height: "856px",
+    width: "28%",
+    background: "#081A2C",
+    border: "0.5px solid #153453",
+    borderRadius: "10px",
+    "@media (max-width:1900px)": {
+        height: "770px",
+        width: "24%",
+    }
+});
+
+const AbsentStaff = () => {
+    return (
+        <ContainerAbsentStaff>
+            
+        </ContainerAbsentStaff>
+    );
+};
+
+export default AbsentStaff;
--- src/pages/Staff/Staff.tsx
+++ src/pages/Staff/Staff.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React from 'react';
+import { styled } from "@mui/system";
+import TotalDeliveryStaff from "src/components/Staff/Content/TotalDeliveryStaff/TotalDeliveryStaff";
+import AbsentStaff from "../../components/Staff/Content/AbsentStaff/AbsentStaff";
+import DeliveredOrders from "../../components/Dashboard/Content/DeliveredOrders/DeliveredOrders";
+
+const ContainerStaff = styled("div")({
+    minHeight: "calc(100vh - 64px)",
+    backgroundColor: "#030f1c",
+});
+
+const StaffBody = styled("div")({
+    marginLeft: "220px",
+    paddingTop: "27px"
+});
+
+const StaffHeader = styled("div")({
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: "18px",
+    lineHeight: "21px",
+    color: "#FFFFFF"
+});
+
+const StaffContent = styled("div")({
+    display: "flex",
+    marginTop: "30px"
+});
 
 const Staff = () => {
-  return <div>Staff</div>;
+    return (
+        <ContainerStaff>
+            <StaffBody>
+                <StaffHeader>
+                    Manage Staff
+                </StaffHeader>
+
+                <StaffContent>
+                    <TotalDeliveryStaff/>
+                    <AbsentStaff/>
+                    <DeliveredOrders/>
+                </StaffContent>
+
+
+            </StaffBody>
+        </ContainerStaff>
+    );
 };
 
 export default Staff;
