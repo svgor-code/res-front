12be3eb0221d7b16de98259a7b234d772b53f8ab
evgeny3322 <retard_x_98@mail.ru>
2023-01-23 20:56:35 +0300

    test 5

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,7 +2,7 @@ import { Route, RouteProps } from "react-router-dom";
 
 export const PrivateRoute = ({ component: Component, ...rest }: RouteProps) => {
   if (!Component) {
-    return <></>; ;
+    return <></>;
   }
 
   return <Route path={rest.path} exact component={Component} />;
