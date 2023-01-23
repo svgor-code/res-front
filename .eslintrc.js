ee6f1211c7736bf8b3b6387f855bca658dc92b34
evgeny3322 <retard_x_98@mail.ru>
2023-01-23 22:18:38 +0300

    Revert "add: pre-commit is configured"
    This reverts commit 06aacb049cddbf608d291da9bb3fb550b58c7417.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,13 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ["node-modules"],
+  ignorePatterns: [".eslintrc.js"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "eslint no-unsafe-finally": "error",
   },
 };
---
+++ src/components/Dashboard/Content/SalesGraph/test.js
@@ -0,0 +1,3 @@
+const testString = 'retry';
+const testString2 = 'retry2'
+
